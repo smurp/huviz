5250945e9d85ee12cd36253d11c3c391236010d8
Shawn Murphy <smurp@smurp.com>
2014-11-18 15:45:51 -0800

    WIP hmm tests seem to work but why is the graph left in the state it is

---
+++ js/huviz_test.js
@@ -0,0 +1,41 @@
+var expect = chai.expect;
+
+describe("HuViz Tests", function() {
+  this.timeout(0);
+  huviz = require('huviz');
+  number_of_nodes = 15;
+
+  before(function bootHuviz() {
+    window.HVZ = new huviz.Orlando();
+    window.HVZ.set_ontology("/data/OrlandoOntology-SexesUnderPerson.ttl");
+    document.addEventListener('dataset-loaded', function(e) {
+      console.log("dataset-loaded",arguments);
+      HVZ.choose_everything();
+    }, false);
+    window.HVZ.boot_sequence();
+  })
+
+  describe("constructor", function() {
+    it("initially everything should be shelved and nothing graphed", function(done) {
+      expect(HVZ.graphed_set.length).to.equal(0);
+      expect(HVZ.shelved_set.length).to.equal(HVZ.nodes.length);
+      done();
+    });
+
+    it("'choose Thing.' should result in non-zero graphed_set.length ", function(done) {
+      HVZ.do({"verbs": ["choose"], "sets": [HVZ.shelved_set]});
+      expect(HVZ.graphed_set.length).to.not.equal(0);
+      expect(HVZ.graphed_set.length).to.equal(number_of_nodes);
+      done();
+    });
+
+    it("'shelve Thing.' should remove everything from the graph ", function(done) {
+      HVZ.do({"verbs": ["shelve"], "sets": [HVZ.graphed_set]});
+      expect(HVZ.graphed_set.length).to.equal(0);
+      expect(HVZ.shelved_set.length).to.equal(number_of_nodes);
+      done();
+    });
+
+  });
+});
+
