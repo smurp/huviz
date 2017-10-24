ff410693037a73778431019e1f13544a3c915080
Shawn Murphy <smurp@smurp.com>
2017-10-24 15:13:23 +0200

    add name and active flag

--- js/oncerunner.js
+++ js/oncerunner.js
@@ -1,13 +1,20 @@
 "use strict";
 
-function OnceRunner(verbosity) {
+function OnceRunner(verbosity, name) {
   this.verbosity = verbosity || 0;
+  this.profileName = (name || 'OnceRunner') + '_' + (new Date()).toISOString();
+  this.active = false;
 };
 
 OnceRunner.prototype.setTimeout = function(cb, msec) {
   //console.log(`afterMsec_call(${msec}) typeof cb ==>`,typeof cb);
   if (! this.firstCallTime) { // when a new string of calls begins
     this.firstCallTime = Date.now();
+    this.active = true;
+    this.stats = {};
+    if (this.verbosity > 39) {
+      console.profile(this.profileName);
+    }
     this.clearCount = 0; // the number of times execution has been delayed
   }
   if (this.timeoutID) {
@@ -30,17 +37,22 @@ OnceRunner.prototype.makeWrapper = function(callback) {
     }
     var callbackExecutionStart = Date.now();
     callback();
+    self.active = false;
     var callbackExecutionEnd = Date.now();
     var callbackExecutionDurationMsec = callbackExecutionEnd - callbackExecutionStart;
     var overallExecutionDurationMsec = callbackExecutionEnd - self.firstCallTime;
     var timeSavedMsec = callbackExecutionDurationMsec * self.clearCount;
     var wouldaBeenDurationMsec = overallExecutionDurationMsec + timeSavedMsec;
     var timeSavedRatio = timeSavedMsec / wouldaBeenDurationMsec;
-    var stats = {'timeSavedRatio': timeSavedRatio,
-                 'timeSavedSec': timeSavedMsec/1000,
-                 'wouldaBeenSec': wouldaBeenDurationMsec / 1000};
+    self.stats.timeSavedRatio = timeSavedRatio;
+    self.stats.timeSavedSec = timeSavedMsec/1000;
+    self.stats.wouldaBeenSec = wouldaBeenDurationMsec / 1000;
+    self.stats.profileName = self.profileName;
     if (self.verbosity > 9) {
-      console.warn("OnceRunner() stats", stats);
+      console.warn("OnceRunner() stats", self.stats);
+    }
+    if (self.verbosity > 39) {
+      console.profileEnd();
     }
     self.firstCallTime = null; // an execution has happened so reset
   };
