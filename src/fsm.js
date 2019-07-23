edbfbe9befa69750719131badf70d0810ae8c62f
Adsidera <annamaria.costalonga@gmail.com>
2019-07-23 21:37:11 +0200

    decaffeinated without noise except for glcui and huviz

---
+++ src/fsm.js
@@ -0,0 +1,113 @@
+/*
+ * decaffeinate suggestions:
+ * DS207: Consider shorter variations of null checks
+ * DS208: Avoid top-level this
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+// FiniteStateMachine implements a simple abstract engine for running state machines.
+//
+// It supports optional methods for every transition and for become/leav a state.
+//
+// There are three kinds of methods:
+// 1. on__TRANSITID called upon the commencement of the transition
+// 2. exit__STATEID called when leaving a state
+// 3. enter__STATEID called when becoming a state
+//
+// All three kinds of methods are optional.  If no method of any kind is found
+// during a transition then a message is either thrown, logged or ignored based
+// on the value of this.throw_log_or_ignore
+//
+// If there is an array at this.trace then the names of the method are pushed
+// onto it as they are called.
+//
+// # Usage
+//
+//     class MyFSM extends FiniteStateMachine
+//       constructor: (anything, you want) ->
+//         # you can do anything you want on your FSM constructor
+//       throw_log_or_ignore: 'ignore'
+//       transitions:
+//         start:
+//           target: 'ready'
+//         stop:
+//           source: 'ready' # TODO respect source by raising error if an illegal transit is tried
+//           target: 'stopped'
+//       on__start: ->
+//         console.log('on "start"')
+//       exit__ready: ->
+//         console.log('leave "ready"')
+//       enter__stopped: ->
+//         console.log('become "stopped"')
+//
+//     myFSM = new MyFSM()
+//     myFSM.transit('start') ==> 'on "start"', 'leave "ready"'
+//     myFSM.get_state() ==> 'ready'
+//     myFSM.transit('stop') ==> 'become "stopped"'
+//
+
+// Notes:
+//   suitable for use as a mixin
+//   https://coffeescript-cookbook.github.io/chapters/classes_and_objects/mixins
+class FiniteStateMachine {
+  call_method_by_name(meth_name) {
+    let meth;
+    if (meth = this[meth_name]) {
+    //if (meth = Reflect.get(this, meth_name))
+      meth.call(this);
+      if (this.trace) {
+        this.trace.push(meth_name);
+      }
+      return true;
+    }
+    return false;
+  }
+  set_state(state) {
+    // call a method when arriving at the new state, if it exists
+    const called = this.call_method_by_name(`enter__${state}`);
+    this.state = state; // set after calling meth_name so the old state is available to it
+    return called;
+  }
+  exit_state() {
+    // call a method when leaving the old state, if it exists
+    return this.call_method_by_name(`exit__${this.state}`);
+  }
+  get_state() {
+    return this.state;
+  }
+  is_state(candidate) {
+    return this.state === candidate;
+  }
+  make_noop_msg(trans_id, old_state, new_state) {
+    return this.constructor.name + " had neither " +
+           `on__${trans_id} exit__${old_state} or enter__${new_state}`;
+  }
+  throw_log_or_ignore_msg(msg) {
+    const throw_log_or_ignore = this.throw_log_or_ignore || 'ignore';
+    if (throw_log_or_ignore === 'throw') {
+      throw new Error(msg);
+    } else if (throw_log_or_ignore === 'log') {
+      console.warn(msg);
+    }
+  }
+  transit(trans_id) {
+    let transition;
+    if (this.transitions == null) { this.transitions = {}; }
+    if (transition = this.transitions[trans_id]) {
+      let target_id;
+      const initial_state = this.state;
+      let called = this.call_method_by_name(`on__${trans_id}`);
+      called = this.exit_state() || called;
+      if (target_id = transition.target) {
+        called = this.set_state(target_id) || called;
+      }
+      if (!called) {
+        const msg = this.make_noop_msg(trans_id, initial_state, target_id);
+        this.throw_log_or_ignore_msg(msg);
+      }
+    } else {
+      this.throw_log_or_ignore_msg(`${this.constructor.name} has no transition with id ${trans_id}`);
+    }
+  }
+}
+
+(typeof exports !== 'undefined' && exports !== null ? exports : this).FiniteStateMachine = FiniteStateMachine;
---
+++ src/treectrl.js
@@ -0,0 +1,206 @@
+/*
+ * decaffeinate suggestions:
+ * DS101: Remove unnecessary use of Array.from
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS207: Consider shorter variations of null checks
+ * DS208: Avoid top-level this
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+/*
+TreeCtrl controls TreePicker states: showing, unshowing, mixed for direct and indirect.
+
+ Elements may be in one of these states:
+   mixed      - some instances of the node class are selected, but not all
+   unshowing  - there are instances but none are selected
+   showing    - there are instances and all are selected
+   abstract   - there are no instances (but presumably there are subs)
+   (empty)    - is empty a better name for taxon with no direct members?
+                Perhaps 'empty' is a legal direct-state and 'abstract' is only
+                sensible as an indirect-state? Do these distinctions make
+                sense in both the Taxon context and the Predicate context?
+
+ What about these theoretical states?
+   hidden     - TBD: not sure when hidden is appropriate
+                perhaps abstract predicate subtrees should be hidden
+                ie "there is nothing interesting here, move along"
+   emphasized - TBD: mark the class of the focused_node
+
+
+ Are these states only meaningful in the MVC View context and not the
+ Model context? -- where the model context is Taxon and/or Predicate
+ while the View context is the TreePicker.  Likewise 'collapse' is
+ concept related to the View.  OK so we have View verbs such as:
+ * hide
+ * emphasize
+ * collapse
+ * pick (click?)
+ and Model verbs such as:
+
+
+*/
+class TreeCtrl {
+  constructor() {
+    this.state = 'empty';
+    this.indirect_state = 'empty';
+    this.subs = [];
+    this.super_class = null;
+    this.direct_stats = [0, 0];
+    this.dirty = false;
+  }
+  get_state() {
+    if ((this.state == null)) {
+      alert(`${this.id} has no direct state`);
+    }
+    return this.state;
+  }
+  get_indirect_state() {
+    if ((this.indirect_state == null)) {
+      alert(`${this.id} has no indirect_state`);
+    }
+    return this.indirect_state;
+  }
+  register_superclass(super_class) {
+    if (super_class === this) {
+      return;
+    }
+    if (this.super_class != null) {
+      this.super_class.remove_subclass(this);
+    }
+    this.super_class = super_class;
+    return this.super_class.register_subclass(this);
+  }
+  remove_subclass(sub_class) {
+    const idx = this.subs.indexOf(sub_class);
+    if (idx > -1) {
+      return this.subs.splice(idx, 1);
+    }
+  }
+  register_subclass(sub_class) {
+    return this.subs.push(sub_class);
+  }
+  recalc_states() {
+    this.direct_stats = this.recalc_direct_stats(); // eg [3, 60]
+    this.indirect_stats = this.recalc_indirect_stats([0, 0]);
+    this.state = this.recalc_direct_state();   // eg empty|unshowing|mixed|showing
+    this.indirect_state = this.recalc_indirect_state(); // same as above
+  }
+  recalc_indirect_state() {
+    //return @translate_stats_to_state @indirect_state
+    if (this.subs.length === 0) {
+      return this.state;      // eg 0/0
+    }
+    if (this.state === 'mixed') {
+      return 'mixed';     // eg 3/6
+    }
+    let consensus = this.get_state(); // variable for legibility and performance
+    for (let kid of Array.from(this.subs)) {
+      const kid_ind_stt = kid.get_indirect_state();
+      //  debugger
+      if (kid_ind_stt !== consensus) {
+        if (['empty', 'hidden'].includes(consensus)) {
+          consensus = kid_ind_stt;
+        } else if (!['empty', 'hidden'].includes(kid_ind_stt)) {
+          return "mixed";
+        }
+      }
+    }
+    return consensus;
+  }
+  set_dirty() {
+    this.dirty = true;
+    if (this.super_class != null) {
+      return this.super_class.set_dirty();
+    }
+  }
+  update_state(inst, change) {
+    if (inst != null) {
+      this.change_map[change].acquire(inst);
+    }
+    return this.set_dirty();
+  }
+    //@fire_changeEvent_if_needed()
+  clean_up_dirt() {
+    if (!this.dirty) {
+      return;
+    }
+    this.dirty = false;
+    // FIXME fold the subroutines into this method for a single pass
+    // FIXME make use of the edge and change hints in the single pass
+    // terminology:
+    //   selected edge:  an edge (shown or not) to or from a node in the selected_set
+    // roughly: all_shown, none_shown, mixed, hidden
+    //   are all the selected edges shown?
+    //   are none of the selected edges shown?
+    //   are strictly some of the selected edges shown?
+    //   are there no selected edges?
+    const old_state = this.state;
+    const old_indirect_state = this.indirect_state;
+
+    for (let kid of Array.from(this.subs)) {
+      kid.clean_up_dirt();
+    }
+    this.recalc_states();
+    const updating_stats = true; // TODO make this settable by user
+    if (updating_stats ||
+        (old_state !== this.state) ||
+        (old_indirect_state !== this.indirect_state)) {
+      if (window.suspend_updates) {
+        return;
+      }
+      const evt = new CustomEvent(this.custom_event_name, {
+          detail: {
+            target_id: this.lid,
+            target: this,
+            old_state,
+            new_state: this.state,
+            old_indirect_state,
+            new_indirect_state: this.indirect_state,
+            payload: this.get_payload_string(),
+            collapsed_payload: this.get_collapsed_payload_string()
+          },
+          bubbles: true,
+          cancelable: true
+        }
+      );
+      return window.dispatchEvent(evt);
+    }
+  }
+      //if @super_class?
+      //  @super_class.fire_changeEvent_if_needed()
+  format_stats(stats) {
+    return `${stats[0]}/${stats[1]}`;
+  }
+  translate_stats_to_state(stats) {
+    if (stats[1] === 0) {
+      return "empty";
+    }
+    if (stats[0] === 0) {
+      return "unshowing";
+    }
+    if (stats[0] === stats[1]) {
+      return "showing";
+    }
+    return "mixed";
+  }
+  recalc_direct_state() {
+    return this.translate_stats_to_state(this.direct_stats);
+  }
+  get_payload_string() {
+    return this.format_stats(this.direct_stats);
+  }
+  get_collapsed_payload_string() {
+    return this.format_stats(this.indirect_stats);
+  }
+  recalc_indirect_stats(stats) {
+    stats[0] += this.direct_stats[0];
+    stats[1] += this.direct_stats[1];
+    if (this.subs.length > 0) {
+      for (let sub of Array.from(this.subs)) {
+        sub.recalc_indirect_stats(stats);
+      }
+    }
+    return stats;
+  }
+}
+
+(typeof exports !== 'undefined' && exports !== null ? exports : this).TreeCtrl = TreeCtrl;
