498bfb9a03e1e22cac330efaa6cf2abdbdb36e06
Shawn Murphy <smurp@smurp.com>
2020-12-02 22:38:38 +0100

    coming along

--- js/historymockup.js
+++ js/historymockup.js
@@ -1,4 +1,6 @@
-var nextVerb, nextNoun;
+var commandHistory, nextVerb, nextNoun;
+var currentCommand = {};
+
 function clickVerb(evt) {
   setNextVerb(evt.target.innerText, true);
 }
@@ -6,10 +8,19 @@ function clickSet(evt) {
   setNextNoun(evt.target.innerText, true);
 }
 function leaveVerb(evt) {
-  setNextVerb("", false);
+  setNextVerb(null, false);
 }
 function leaveSet(evt) {
-  setNextNoun("", false);
+  if (!currentCommand.nounReady) {
+    setNextNoun(null, false);
+  }
+}
+function maybeExecuteCommand() {
+  if (currentCommand.verb && currentCommand.noun) {
+    var command = currentCommand.verb + ' ' + currentCommand.noun + ' .';
+    commandHistory.insertAdjacentHTML('afterbegin', `<div class="played command">${command}</div>`);
+  }
+  console.log(currentCommand);
 }
 function overVerb(evt) {
   setNextVerb(evt.target.innerText);
@@ -17,13 +28,27 @@ function overVerb(evt) {
 function overSet(evt) {
   setNextNoun(evt.target.innerText);
 }
-function setNextVerb(val, perm) {
-  nextVerb.innerText = val;
-  nextVerb.class = perm && 'perm' || '';
+function setNext(elem, val, ready) {
+  elem.innerText = val;
+  if (ready) {
+    elem.classList.add('nextcommand_prompt_ready');
+    elem.classList.remove('nextcommand_prompt_unready');
+  } else {
+    elem.classList.add('nextcommand_prompt_unready');
+    elem.classList.remove('nextcommand_prompt_ready');
+  }
 }
-function setNextNoun(val, perm) {
-  nextNoun.innerText = val;
-  nextNoun.class = perm && 'perm' || '';
+function setNextVerb(val, ready) {
+  currentCommand.verb = val;
+  currentCommand.verbReady = ready;
+  setNext(nextVerb, val, ready);
+  maybeExecuteCommand();
+}
+function setNextNoun(val, ready) {
+  currentCommand.noun = val;
+  currentCommand.nounReady = ready;
+  setNext(nextNoun, val, ready);
+  maybeExecuteCommand();
 }
 function onreadyHandler() {
   console.log('onreadyHandler');
@@ -37,7 +62,8 @@ function onreadyHandler() {
     aSet.onmouseleave = leaveSet;
     aSet.onmouseover = overSet;
   });
-  nextVerb = document.querySelector('#nextVerb');
-  nextNoun = document.querySelector('#nextNoun');
+  nextVerb = document.querySelector('.verb_phrase'); // get just first instance of class
+  nextNoun = document.querySelector('.noun_phrase'); // get just first instance of class
+  commandHistory = document.querySelector('.commandHistory');
 }
 window.onload = onreadyHandler;
