f466f1035bf9b95ff24bb1c4839b55a1495493e7
Shawn Murphy <smurp@smurp.com>
2020-12-03 22:17:56 +0100

    make historymockup good WIP underlines should be for temp

--- js/historymockup.js
+++ js/historymockup.js
@@ -1,8 +1,8 @@
-var allcommands, commandHistory, nextCommand, nextNoun, nextVerb;
+var allcommands, commandHistory, nextCommand, nextcommand_prompt;
 var currentCommand = {perm:{}, temp:{}};
 
 function onreadyHandler() {
-  console.log('onreadyHandler');
+  //console.log('onreadyHandler');
   document.querySelectorAll(".verbs > button").forEach((aVerb) => {
     aVerb.onclick = handleEvery;
     aVerb.onmouseleave = handleEvery;
@@ -14,14 +14,14 @@ function onreadyHandler() {
     aSet.onmouseover = handleEvery;
   });
   // get just first instances of the mentioned classes
-  nextVerb = document.querySelector('.verb_phrase'); 
-  nextNoun = document.querySelector('.noun_phrase');
   currentCommand.elem = {
-    noun: nextNoun,
-    verb: nextVerb};
+    noun: document.querySelector('.noun_phrase'),
+    verb: document.querySelector('.verb_phrase')
+  };
   commandHistory = document.querySelector('.commandHistory');
   nextCommand = document.querySelector('.nextcommand');
   allcommands = document.querySelector('.allcommands');
+  nextcommand_prompt = document.querySelector('.nextcommand_prompt');
 }
 
 function handleEvery(evt) {
@@ -40,7 +40,7 @@ function setNext(speech, val="", event) {
     currentCommand.temp[speech] = val;
     break
   case 'click':
-    // By clicking the user is conveying they choose this one.
+    // Clicking expresses the user's desire to choose this val.
     // Unless this val had already been the choosen one, then unchoose it.
     if (currentCommand.perm[speech] == val) {
       currentCommand.perm[speech] = null;
@@ -50,15 +50,16 @@ function setNext(speech, val="", event) {
     currentCommand.temp[speech] = null;
     break;
   case 'mouseleave':
+    currentCommand.temp[speech] = null;
     newVal = currentCommand.perm[speech] || null; // fall back to the perm value
     break;
   default:
-    // completely wipe the part of speech like: setNext('verb')
+    // Completely wipe the part of speech like: setNext('verb')
     newVal = '';
     currentCommand.perm[speech] = null;
     currentCommand.temp[speech] = null;
   }
-  console.log('setNext', {speech, val, event, newVal});
+  //console.log('setNext', {speech, val, event, newVal});
   elem.innerText = newVal;
   if (newVal) {
     elem.classList.add('nextcommand_prompt_ready');
@@ -74,19 +75,30 @@ function maybeExecute(speech) {
   var cc = currentCommand;
   var noun = cc.perm.noun || cc.temp.noun;
   var verb = cc.perm.verb || cc.temp.verb;
+  //console.log("maybeExecute", {verb, noun}, cc);
   if (cc.perm.noun && cc.perm.verb) {
-    var command = verb + ' ' + noun + ' .';
-    nextCommand.insertAdjacentHTML(
-      'beforebegin',
-      `<div class="played command">${command}</div>`);
-    setNext(speech);
+    //console.log('execute');
+    execute(speech, verb, noun);
+    console.clear();
   } else if (verb && noun) {
-    nextCommand.classList.add('ready');
+    //console.log('ready');
+    nextcommand_prompt.classList.add('ready');
+    nextcommand_prompt.classList.remove('unready');
   } else {
-    nextCommand.classList.add('unready');
+    //console.log('unready');
+    nextcommand_prompt.classList.add('unready');
+    nextcommand_prompt.classList.remove('ready');
   }
   //console.log("maybeExecute", currentCommand);
   nextCommand.scrollIntoView();
+}
+
+function execute(speech, verb, noun) {
+  var command_str = verb + ' ' + noun + ' .';
+  nextCommand.insertAdjacentHTML(
+    'beforebegin',
+    `<div class="played command">${command_str}</div>`);
+  setNext(speech); // knock out the recently clicked part of speech
 }
 
 window.onload = onreadyHandler;
