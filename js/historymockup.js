43df4a3b570f44e998b36fc3418aec480dd14892
Shawn Murphy <smurp@smurp.com>
2020-12-15 19:30:22 +0100

    implement triple-toggle in historymockup WIP height issue AND antagonisms

--- js/historymockup.js
+++ js/historymockup.js
@@ -1,12 +1,47 @@
 var allcommands, commandHistory, grow_shrink, nextCommand, nextcommand_prompt;
 var currentCommand = {perm:{}, temp:{}};
 
+var untargetFor = {};
+var cancelElemForVerb = {};
+
+var verbPairs = [
+  ['Activate', 'Deactivate'],
+  ['Select', 'Deselect'],
+  ['Label', 'Unlabel'],
+  ['Discard', 'Retrieve'],
+  ['Pin', 'Unpin']];
+
 function onreadyHandler() {
   //console.log('onreadyHandler');
-  document.querySelectorAll(".verbs > button").forEach((aVerb) => {
-    aVerb.onclick = handleEvery;
-    aVerb.onmouseleave = handleEvery;
-    aVerb.onmouseover = handleEvery;
+  var mostRecentVerb = '';
+  var recentVerbs = [];
+  document.querySelectorAll(".verbs  button").forEach((verbElem) => {
+    var text = verbElem.innerText.replace(/^\s+|\s+$/g, '')
+    if (text == 'X') { // one of the cancel buttons
+      var xButt = verbElem;
+      xButt.onclick = (function(aVerbId, xButtElem){
+        return () => disengageToggler(aVerbId, xButtElem);
+      })(mostRecentVerb, xButt);
+      recentVerbs.forEach((aVerbId) => {
+        cancelElemForVerb[aVerbId] = xButt;
+        disengageCancelButton(aVerbId)
+      });
+      recentVerbs = [];
+    } else {
+      var aVerb = verbElem;
+      console.log(aVerb.innerText);
+      aVerb.onclick = handleEvery;
+      aVerb.onmouseleave = handleEvery;
+      aVerb.onmouseover = handleEvery;
+      aVerb.id = text;
+      recentVerbs.push(text);
+      mostRecentVerb = text;
+    }
+  });
+  verbPairs.forEach((pair) => {
+    var [a, b] = pair;
+    untargetFor[a] = b; //document.querySelect(`#${b}`);
+    untargetFor[b] = a; //document.querySelect(`#${a}`);
   });
   document.querySelectorAll(".sets > button").forEach((aSet) => {
     aSet.onclick = handleEvery;
@@ -27,13 +62,50 @@ function onreadyHandler() {
 }
 
 function handleEvery(evt) {
-  var speech = evt.target.parentNode.dataset.speech, // noun | verb
+  var speech = evt.target.closest('[data-speech]').dataset.speech, // noun | verb
       val = evt.target.innerText, // Actviate | Pin | Graphed | etc
       event = evt.type; // click | mouseenter | mouseleave
-  setNext(speech, val, event);
+  setNext(speech, val, event, evt.target);
 }
 
-function setNext(speech, val="", event) {
+function disengageToggler(aVerbId, xButtElem) {
+  var toDisengage = [xButtElem];
+  var untargetId = untargetFor[aVerbId];
+  toDisengage.push(document.getElementById(aVerbId));
+  if (untargetId) {
+    toDisengage.push(document.getElementById(untargetId));
+  }
+  toDisengage.forEach((elem) => {
+    elem.classList.remove('engaged');
+  });
+}
+
+function disengageOther(me) {
+  var other = document.getElementById(untargetFor[me.id]);
+  if (other) {
+    other.classList.remove('engaged');
+  }
+}
+
+function engageCancelButton(verbId) {
+  var xButt = cancelElemForVerb[verbId];
+  if (xButt) {
+    xButt.classList.add('engaged');
+  }
+}
+function disengageCancelButton(verbId) {
+  var xButt = cancelElemForVerb[verbId];
+  if (xButt) {
+    xButt.classList.remove('engaged');
+  }
+}
+
+function setNext(speech, val="", event, target) {
+  if (event == 'click') {
+    var perm = currentCommand.perm.verb;
+    var temp = currentCommand.temp.verb;
+    console.log({speech, val, event, perm, temp});
+  }
   var elem = currentCommand.elem[speech];
   var newVal = val;
   switch (event) {
@@ -46,8 +118,13 @@ function setNext(speech, val="", event) {
     // Unless this val had already been the choosen one, then unchoose it.
     if (currentCommand.perm[speech] == val) {
       currentCommand.perm[speech] = null;
+      target.classList.remove('engaged');
+      disengageCancelButton(target.id);
     } else {
       currentCommand.perm[speech] = val;
+      target.classList.add('engaged');
+      engageCancelButton(target.id);
+      disengageOther(target);
     }
     currentCommand.temp[speech] = null;
     break;
@@ -114,4 +191,24 @@ function handleGrowShrink(evt) {
   }
 }
 
+function handleClick(event) {
+  switch (event.target.id) {
+  case 'topbutton':
+    topbutton.classList.toggle("button-pressed");
+    bottombutton.classList.remove("button-pressed");
+    break;
+  case 'bottombutton':
+    bottombutton.classList.toggle("button-pressed");
+    topbutton.classList.remove("button-pressed");
+    break;
+  case 'xbutton':
+    topbutton.classList.remove("button-pressed");
+    bottombutton.classList.remove("button-pressed");
+    break;
+  }
+  console.log(event.target);
+}
+
 window.onload = onreadyHandler;
+
+
