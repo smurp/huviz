291cdab3f8d1dac9dbc675c57bdb41a3acb20561
ilya <ilyadorosh@gmail.com>
2021-08-10 19:08:47 +0300

    add setting

--- src/huviz.js
+++ src/huviz.js
@@ -1522,11 +1522,10 @@ Link details may not be accurate. Activate to load.</i>`; // """
       this.canvas.height = this.height;
     }
     let radius = this.graph_radius;
-    let centering_force_strength = 0.02; // TODO make this a setting
     this.d3simulation.force(
-      "x", d3.forceX(radius).strength(centering_force_strength));
+      "x", d3.forceX(radius).strength(this.centeringForceStrength));
     this.d3simulation.force(
-      "y", d3.forceY(radius).strength(centering_force_strength));
+      "y", d3.forceY(radius).strength(this.centeringForceStrength));
     if (this.d3forceCenter) {
       this.d3forceCenter = this.update_d3forceCenter();
     }
