86e06a171443714f05256371771efd0d2eee249a
Shawn Murphy <smurp@smurp.com>
2022-04-04 11:49:53 +0200

    shorten sparql_timeout to 5sec

--- src/settings.js
+++ src/settings.js
@@ -948,9 +948,9 @@ If zero before loading, then lookup is suppressed.`
         title: "Number of seconds to run SPARQL queries before giving up."
       },
       input: {
-        value: 45,
+        value: 5,
         min: 1,
-        max: 90,
+        max: 60,
         step: 1,
         type: "range"
       }
